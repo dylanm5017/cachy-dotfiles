07b082b5590f8b05684969f261cd862edc582a87
Dylan <84342823+DylanAnderson-Tanduo@users.noreply.github.com>
2026-04-19 14:30:00 -0500

    switched themes & style

--- .config/zsh/history.zsh
+++ .config/zsh/history.zsh
@@ -1,7 +1,12 @@
 HISTSIZE=10000
 SAVEHIST=10000
+HISTFILE=~/.zsh_history   # make sure this is set
+
 setopt HIST_IGNORE_ALL_DUPS
-setopt SHARE_HISTORY
 setopt HIST_FIND_NO_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_VERIFY
+
+setopt SHARE_HISTORY      # share across sessions
+setopt INC_APPEND_HISTORY # write commands immediately
+setopt APPEND_HISTORY     # append instead of overwrite
